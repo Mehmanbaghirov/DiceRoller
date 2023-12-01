61f1b43433f139dc160121b03dfc2bb7808868ff
mehmanbaghirov <88742092+Mehmanbaghirov@users.noreply.github.com>
2023-12-01 09:25:23 +0400

    Update Dice.js

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,25 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../Dice.css";
 
-const Dice = ({ sides, onRoll }) => {
+const Dice = ({ onRoll, image }) => {
   const handleClick = () => {
-    const randomRoll = Math.floor(Math.random() * sides) + 1;
-    onRoll(randomRoll);
+    onRoll();
   };
 
   return (
     <div className="dice-container">
       <div className="dice" onClick={handleClick}>
-        {<img src=""></img>}
-        {sides}-tərəfli zər
+        <img src={image} alt="dice" />
       </div>
     </div>
   );
 };
 
 Dice.propTypes = {
-  sides: PropTypes.number.isRequired,
   onRoll: PropTypes.func.isRequired,
+  image: PropTypes.string.isRequired,
 };
 
 export default Dice;
