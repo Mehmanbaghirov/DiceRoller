01ce33da8cb7eb4a36e501a25a98a677c893c09d
mehmanbaghirov <88742092+Mehmanbaghirov@users.noreply.github.com>
2023-12-01 09:24:42 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Dice from "./components/Dice";
+import dice1 from "./images/dice.png";
+import dice2 from "./images/dice2.png";
+import dice3 from "./images/dice3.png";
+import dice4 from "./images/dice4.png";
+import dice5 from "./images/dice5.png";
+import dice6 from "./images/dice6.png";
 
-function App() {
+const diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
+
+const App = () => {
+  const [diceImage, setDiceImage] = useState(dice1);
+
+  const handleRoll = () => {
+    const randomRoll = Math.floor(Math.random() * 6);
+    setDiceImage(diceImages[randomRoll]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Dice sides={6} onRoll={handleRoll} image={diceImage} />
     </div>
   );
-}
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
